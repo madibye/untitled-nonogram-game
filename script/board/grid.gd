90fd22e5be1d7a7040cc62988709279a621951a8
Madi Simpson <madibyee@gmail.com>
2024-06-12 09:43:12 -0700

    grid/square: add row number display

--- script/board/grid.gd
+++ script/board/grid.gd
@@ -1,6 +1,8 @@
 class_name Grid
 extends GridContainer
 
+const SquareState = Square.SquareState
+
 @onready var squares: Array[Square] = []
 @export var rows: int
 @export var cols: int
@@ -11,29 +13,46 @@ func _ready():
 	populate_squares()
 
 func populate_squares(r = rows, c = cols):
+	set_grid_size(r, c)
 	while squares.size() < (rows*cols):
 		squares.append(make_square())
-	for s in squares:
-		add_child(s)
-	set_grid_size(r, c)
 	
 	row_numbers = range(rows).map(generate_row_numbers.bind('y'))
 	col_numbers = range(cols).map(generate_row_numbers)
+	var max_rn = row_numbers.map(func(rn): return rn.size()).max()
+	var max_cn = col_numbers.map(func(rn): return rn.size()).max()
+	var adjusted_rows = rows + max_cn
+	set_columns(columns + max_rn)
+	print(max_rn)
+	print(max_cn)
+	print(adjusted_rows)
+	print(columns)
 	
-	print(row_numbers)
-	print(col_numbers)
-		
+	for i in range(adjusted_rows * columns):
+		var pos = Vector2((i % columns) - max_rn, floor(i / (columns)) - max_cn)
+		var sq = preload("res://scene/square.tscn").instantiate() as Square
+		sq.set_sq_state(SquareState.BLANK)
+		sq.set_pos(pos.x, pos.y)
+		var filtered = flatten_array([squares, row_numbers, col_numbers]).filter(func(s): return Vector2(s.x_pos, s.y_pos) == pos)
+		if filtered:
+			sq = filtered[0]
+		if sq.get_parent():
+			sq.get_parent().remove_child(sq)
+		add_child(sq)
+	return
+	
 func make_square() -> Square:
 	var sq = preload("res://scene/square.tscn").instantiate() as Square
-	sq.set_pos(squares.size() % 10, floor(squares.size() / 10))
+	sq.set_pos(squares.size() % cols, floor(squares.size() / cols))
+	print(squares.size() % cols, floor(squares.size() / cols))
 	return sq
 
 func set_grid_size(r, c) -> void:
 	rows = r
 	cols = c
-	set_columns(cols)
+	set_columns(c)
 	
-func generate_row_numbers(row, pos_identifier: String = 'x'):
+func generate_row_numbers(row, pos_identifier: String = 'x') -> Array:
 	var relevant_squares = squares.filter(
 		func(sq: Square): return sq.get('%s_pos' % pos_identifier) == row)
 	var rn = []
@@ -47,4 +66,31 @@ func generate_row_numbers(row, pos_identifier: String = 'x'):
 		last_was_filled = true
 	if not rn:
 		rn.push_back(0)
-	return rn
+	var side_facing = pos_identifier.contains('y')
+	var enum_offset = 4 if side_facing else 0
+	var opposite_letter = 'x' if side_facing else 'y'
+	var rn_squares = rn.map(func(r): 
+		var sq: Square = preload("res://scene/square.tscn").instantiate() as Square
+		sq.set_sq_state(SquareState.BLUE_LINES + enum_offset)
+		sq.set_label_text("[center][color=#000000]%s[/color][/center]" % r)
+		sq.set('%s_pos' % pos_identifier, row)
+		return sq
+	)
+	rn_squares[0].set_sq_state(SquareState.FADING_BLUE_LINES + enum_offset)
+	rn_squares[-1].set_sq_state((SquareState.BOTTOM_FADING_BLUE_LINES if rn_squares.size() == 1 else \
+								 SquareState.BOTTOM_BLUE_LINES) + enum_offset)
+	for i in range(rn_squares.size()):
+		rn_squares[i].set('%s_pos' % opposite_letter, i - rn_squares.size())
+	return rn_squares
+	
+func find_square_from_pos(list, pos):
+	return list.filter(func(i): return Vector2(i.x_pos, i.y_pos) == pos)
+
+func flatten_array(list) -> Array:
+	var ret_list = []
+	for i in list:
+		if i is Array:
+			ret_list.append_array(flatten_array(i))
+		else:
+			ret_list.append(i)
+	return ret_list
--- script/board/square.gd
+++ script/board/square.gd
@@ -1,35 +1,36 @@
 class_name Square
 extends TextureButton
 
-enum SquareState {NORMAL, X_MARKED, O_MARKED, REVEALED, FADING_BLUE_LINES, BLUE_LINES, BOTTOM_BLUE_LINES, BLANK}
+enum SquareState {NORMAL, X_MARKED, O_MARKED, REVEALED, FADING_BLUE_LINES, BLUE_LINES, BOTTOM_BLUE_LINES, 
+				  BOTTOM_FADING_BLUE_LINES, SIDE_FADING_BLUE_LINES, SIDE_BLUE_LINES, SIDE_BOTTOM_BLUE_LINES, 
+				  SIDE_BOTTOM_FADING_BLUE_LINES, BLANK}
 
 var x_pos = 0
 var y_pos = 0
 
-@onready var is_filled = bool(randi() % 2)
+var is_filled = bool(randi() % 2)
 @onready var revealed = false
 @onready var sq_state = SquareState.NORMAL
-@onready var clickable = true
 
 func _on_gui_input(event: InputEvent):
-	if not clickable or not event is InputEventMouseButton or (
+	if (x_pos < 0 or y_pos < 0) or (
+		not event is InputEventMouseButton) or (
 		not event.pressed
 	):
 		return
-	print(is_filled)
 	set_sq_state({
 		1: sq_state if not is_filled else SquareState.REVEALED, 2: SquareState.X_MARKED,
-	}.get(event.button_index, SquareState.O_MARKED) if not sq_state == SquareState.REVEALED else SquareState.REVEALED)
+	}.get(event.button_index, SquareState.O_MARKED) if sq_state <= SquareState.O_MARKED else sq_state)
 
-func set_sq_state(state: SquareState, rotate: int = 0):
+func set_sq_state(state: SquareState):
 	sq_state = state
-	update_texture_region(rotate)
+	update_texture_region()
 	
 func set_pos(x, y):
 	x_pos = x
 	y_pos = y
 
-func update_texture_region(rotate: int = 0):
+func update_texture_region():
 	# All of the texture mapping math happens here.
 	var region_size: Vector2 = texture_normal.region.size
 	var atlas_size: Vector2 = texture_normal.atlas.get_size()
@@ -37,4 +38,7 @@ func update_texture_region(rotate: int = 0):
 		int(sq_state * region_size.x) % int(atlas_size.x), 
 		floor((sq_state * region_size.x) / atlas_size.x) * region_size.x, 
 		region_size.x, region_size.y)
-	rotation_degrees = rotate
+
+func set_label_text(text: String):
+	$NumLabel.text = text
+	
